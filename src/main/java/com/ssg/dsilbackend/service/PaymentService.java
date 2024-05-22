b6b2b19771acfdea8e4626fdcefca43b947ed5c1
Imhwan <ynw2341@naver.com>
2024-05-23 06:00:14 +0900

    회원 보유 포인트 (currentPoint) 차감 구현

--- src/main/java/com/ssg/dsilbackend/service/PaymentService.java
+++ src/main/java/com/ssg/dsilbackend/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.ssg.dsilbackend.service;
 
 import com.ssg.dsilbackend.domain.Members;
 import com.ssg.dsilbackend.domain.Payment;
+import com.ssg.dsilbackend.domain.Point;
 import com.ssg.dsilbackend.domain.Reservation;
 import com.ssg.dsilbackend.dto.PaymentStatus;
 import com.ssg.dsilbackend.dto.payment.PaymentDTO;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 
 @Service
@@ -23,6 +25,7 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final ReservationRepository reservationRepository;
     private final RefundService refundService;
+    private final PointManageRepository pointManageRepository;
 
     public void savePayment(PaymentDTO paymentDTO, Long reservationId) {
         try {
@@ -35,25 +38,37 @@ public class PaymentService {
             String email = members.getEmail();
             String tel = members.getTel();
             String name = members.getName();
-            Payment payment = Payment.builder()
-                    .amount(paymentDTO.getAmount())
-                    .paymentTime(LocalDateTime.now())
-                    .pg(paymentDTO.getPg())
-                    .buyerEmail(email)
-                    .buyerName(name)
-                    .buyerTel(tel)
-                    .payMethod(paymentDTO.getPay_method())
-                    .paymentTime(LocalDateTime.now())
-                    .name(paymentDTO.getName())
-                    .merchantUid(paymentDTO.getMerchant_uid())
-                    .reservation(reservation)
-                    .paymentStatus(PaymentStatus.COMPLETED)
-                    .pointUsage(paymentDTO.getPointUsage())
-                    .impUid(paymentDTO.getImpUid())
-                    .build();
 
-            paymentRepository.save(payment);
+            Point point = members.getPoint();
 
+            Long pointUsage = paymentDTO.getPointUsage();
+
+            if (point.getCurrentPoint() >= pointUsage) {
+                point.setCurrentPoint(point.getCurrentPoint() - pointUsage); // 사용한 포인트 차감
+                pointManageRepository.save(point); // 포인트 정보 저장
+
+                Payment payment = Payment.builder()
+                        .amount(paymentDTO.getAmount())
+                        .paymentTime(LocalDateTime.now())
+                        .pg(paymentDTO.getPg())
+                        .buyerEmail(email)
+                        .buyerName(name)
+                        .buyerTel(tel)
+                        .payMethod(paymentDTO.getPay_method())
+                        .paymentTime(LocalDateTime.now())
+                        .name(paymentDTO.getName())
+                        .merchantUid(paymentDTO.getMerchant_uid())
+                        .reservation(reservation)
+                        .paymentStatus(PaymentStatus.COMPLETED)
+                        .pointUsage(paymentDTO.getPointUsage())
+                        .impUid(paymentDTO.getImpUid())
+                        .build();
+
+                paymentRepository.save(payment);
+
+            } else {
+                throw new RuntimeException("포인트 부족 무튼 오류임");
+            }
         } catch (Exception e) {
             log.error(e);
         }
