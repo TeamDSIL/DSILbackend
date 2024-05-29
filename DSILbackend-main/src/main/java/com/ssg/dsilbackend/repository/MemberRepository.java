f8c20ba5fc872738e0a3159de386fbd70d8fa00b
ImHwan <ynw2341@naver.com>
2024-05-29 15:04:40 +0900

    Delete DSILbackend-main directory

--- DSILbackend-main/src/main/java/com/ssg/dsilbackend/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssg.dsilbackend.repository;
-
-import com.ssg.dsilbackend.domain.Members;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Members, Long> {
-}
