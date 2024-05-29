672293546598cb0836e67d420e8da81bcc0f66a1
Moon Sang Hyun <147921376+MSH0512@users.noreply.github.com>
2024-05-29 15:14:12 +0900

    Update Bookmark.java

--- src/main/java/com/ssg/dsilbackend/domain/Bookmark.java
+++ src/main/java/com/ssg/dsilbackend/domain/Bookmark.java
@@ -11,10 +11,11 @@ import lombok.*;
 @ToString
 @Table(name = "bookmark")
 public class Bookmark {
-    @Id
+   @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "bookmarks_id")
     private Long id;
+
 
     @ManyToOne
     @JoinColumn(name = "member_id", nullable = false)
@@ -23,6 +24,14 @@ public class Bookmark {
     @ManyToOne
     @JoinColumn(name = "restaurant_id", nullable = false)
     private Restaurant restaurant;
+    // Constructor - 즐겨찾기 생성 
+    public Bookmark(Members members, Restaurant restaurant) {
+        this.members = members;
+        this.restaurant = restaurant;
+    }
+
+    // Default constructor - 즐겨찾기 삭제
+    protected Bookmark() {}
 
 
 }
