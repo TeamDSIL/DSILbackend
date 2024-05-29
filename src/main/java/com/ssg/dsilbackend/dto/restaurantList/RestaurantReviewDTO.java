989c7b924f96717221d90ee8cc700ff4867631a1
Moon Sang Hyun <147921376+MSH0512@users.noreply.github.com>
2024-05-29 15:20:58 +0900

    Update RestaurantReviewDTO.java

--- src/main/java/com/ssg/dsilbackend/dto/restaurantList/RestaurantReviewDTO.java
+++ src/main/java/com/ssg/dsilbackend/dto/restaurantList/RestaurantReviewDTO.java
@@ -12,11 +12,14 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RestaurantReviewDTO {
-    private Long restaurant_id;
+ private Long restaurant_id;
     private Long id; // 리뷰 아이디
     private String content;
     private LocalDate registerDate;
     private Double score;
+    private String review_img;
+    private String name; // 예약테이블 예약자명
     private Double averageScore;
     private Long reviewCount;
+    private List<Long> ratingsCount;
 }
