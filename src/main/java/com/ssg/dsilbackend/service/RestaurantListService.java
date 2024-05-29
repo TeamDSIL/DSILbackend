61a73b635cb0d31fbfe031f8d172622501bad29b
Moon Sang Hyun <147921376+MSH0512@users.noreply.github.com>
2024-05-29 15:11:48 +0900

    Update RestaurantListService.java

--- src/main/java/com/ssg/dsilbackend/service/RestaurantListService.java
+++ src/main/java/com/ssg/dsilbackend/service/RestaurantListService.java
@@ -9,12 +9,14 @@ import java.util.List;
 
 
 public interface RestaurantListService {
-    List<RestaurantDetailDTO> findMenus(Long id); // 메뉴 리스트 조회
-//    List<MenuListDTO> findRestaurantDetail (Long id);
-
+  List<RestaurantDetailDTO> findMenus(Long id); // 메뉴 리스트 조회
+    //    List<MenuListDTO> findRestaurantDetail (Long id);
     List<RestaurantReviewDTO> getRestaurntReviewsListById(Long restaurantId);
     //    List<RestaurantListDTO> findByCategoryName(CategoryName categoryName);
-    List<RestaurantListDTO> findByCategoryNames(List<CategoryName> categoryNames);
+//    List<RestaurantListDTO> findByCategoryNames(List<CategoryName> categoryNames);
+    List<RestaurantListDTO> findByFilters(List<CategoryName> categoryNames, List<FacilityName> facilities, String search);
+    void addBookmark(Long memberId, Long restaurantId);
+    void removeBookmark(RestaurantBookmarkDTO restaurantBookmarkDTO);
 
 
     void incrementViewCount(Long id);  // 뷰 카운트 증가
